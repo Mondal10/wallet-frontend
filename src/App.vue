1168942bc3b10b3e9f65a32ede5b10fab645f6fa
Amit Mondal <amitmondal111.am@gmail.com>
2026-02-15 14:07:59 +0530

    feat: provide walletId to all child component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
-<script setup></script>
+<script setup>
+import { provide } from 'vue';
+import { getItem } from '~/common/utils/localStorage';
+
+const walletId = getItem('walletId');
+provide('walletId', walletId);
+</script>
 
 <template>
   <router-view />
